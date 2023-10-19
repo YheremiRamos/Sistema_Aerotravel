9001aec7f4faf0d361c288de82b23d01f7b64a03
Yheremi Ramos <143280839+YheremiRamos@users.noreply.github.com>
2023-10-19 15:45:08 -0500

    Add files via upload

---
+++ src/main/java/com/sistemAertravel/adsII/SistemaAerotravelApplication.java
@@ -0,0 +1,13 @@
+package com.sistemAertravel.adsII;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAerotravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAerotravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistemAertravel/adsII/SistemaAerotravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemAertravel.adsII;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAerotravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
